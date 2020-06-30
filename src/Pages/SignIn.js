019538b218deb042567831abaca6bac7a8126348
Zaid Akhter <zaidakhter1202@gmail.com>
2020-06-30 16:42:37 +0500

    📦 NEW: Add sign in page

---
+++ src/Pages/SignIn.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SignIn = () => {
+  return <div className="sign-in-page">SIGN IN</div>;
+};
+
+export default SignIn;
--- src/UnAuthApp.js
+++ src/UnAuthApp.js
@@ -4,6 +4,7 @@ import { Switch, Route } from "react-router-dom";
 import { Navbar } from "./Components/UnAuth";
 import Home from "./Pages/Home";
 import NotFound from "./Pages/404";
+import SignIn from "./Pages/SignIn";
 
 const UnAuthApp = () => {
   return (
@@ -11,6 +12,7 @@ const UnAuthApp = () => {
       <Navbar />
       <Switch>
         <Route exact path="/" children={<Home />} />
+        <Route exact path="/sign-in" children={<SignIn />} />
 
         {/* This stays in the end */}
         <Route path="/*" children={<NotFound />} />
