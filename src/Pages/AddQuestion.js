525b5d782d8998d91da33c8ebaa0efdb6963db7a
Zaid Akhter <zaidakhter1202@gmail.com>
2020-07-05 23:14:27 +0500

    📦 NEW: Simple Slate Editor

--- src/Pages/AddQuestion.js
+++ src/Pages/AddQuestion.js
@@ -1,9 +1,34 @@
-import React from "react";
+// Import React dependencies.
+import React, { useEffect, useMemo, useState } from "react";
+// Import the Slate editor factory.
+import { createEditor } from "slate";
+
+// Import the Slate components and React plugin.
+import { Slate, Editable, withReact } from "slate-react";
 
 const AddQuestion = () => {
+  const [value, setValue] = useState([
+    {
+      type: "paragraph",
+      children: [{ text: "A line of text in a paragraph." }],
+    },
+  ]);
+
+  const editor = useMemo(() => withReact(createEditor()), []);
+
   return (
-    <div className="add-question">
+    <div className="add-question" style={{ backgroundColor: "#f5f5f5" }}>
       <h1>Add Question</h1>
+      <Slate
+        editor={editor}
+        value={value}
+        onChange={newValue => setValue(newValue)}
+      >
+        <Editable
+          style={{ border: "1px solid #d9d9d9", padding: 10 }}
+          className="add-question-field"
+        />
+      </Slate>
     </div>
   );
 };
