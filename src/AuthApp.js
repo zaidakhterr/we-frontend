75fac746ef131da1653c4df3d413313904986986
adeena <adeenalathiya@gmail.com>
2020-07-10 00:49:20 +0500

    Some Changes

--- src/AuthApp.js
+++ src/AuthApp.js
@@ -3,6 +3,7 @@ import { Switch, Route } from "react-router-dom";
 
 import AuthNavbar from './Components/Layout/AuthNavbar';
 import Home from "./Pages/Home";
+import Profile from "./Pages/Profile";  
 import NotFound from "./Pages/404";
 
 const AuthApp = () => {
@@ -11,6 +12,7 @@ const AuthApp = () => {
       <AuthNavbar />
       <Switch>
         <Route exact path="/" children={<Home />} />
+        <Route exact path="/profile" children={<Profile />} />
 
         {/* This stays in the end */}
         <Route path="/*" children={<NotFound />} />
