d1ebe770bcd267666e209c9612f8bd43411a9281
Zaid Akhter <zaidakhter1202@gmail.com>
2020-07-15 11:24:19 +0500

    🐛 FIX: useAuth

--- src/Hooks/useAuth.js
+++ src/Hooks/useAuth.js
@@ -4,12 +4,11 @@ import api from "./../api";
 
 const useAuth = () => {
   const [auth, setAuth] = useState(
-    () => window.localStorage.getItem("nf-auth") || null
+    () => JSON.parse(window.localStorage.getItem("nf-auth")) || null
   );
 
   useEffect(() => {
     window.localStorage.setItem("nf-auth", auth);
-    console.log(auth);
 
     if (auth && auth.status) {
       const { token } = auth;
