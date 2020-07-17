2c8cf7a280886f43a967a5f948691bc54c6f34f7
adeena <adeenalathiya@gmail.com>
2020-07-17 12:18:59 +0500

    ..

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,9 +1,25 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import useAuth from "../Hooks/useAuth";
+
+import { Typography, Row, Col, Button } from 'antd';
+//import Questions from "../Components/Home/Questions";
+
+const { Title } = Typography;
 
 const Home = () => {
+  const { auth } = useAuth();
   return (
     <div className="home-page">
-      <h1>HOME</h1>
+      <div className="container">
+        <Row type="flex" justify="space-between" align="middle">
+          <Col><Title>Top Questions</Title></Col>
+          <Col>{auth && auth.status 
+          ? <Link to="/question/ask"><Button type="primary">Ask Question</Button></Link> 
+          : <h1>hello</h1>}
+          </Col>
+        </Row>
+      </div>
     </div>
   );
 };
