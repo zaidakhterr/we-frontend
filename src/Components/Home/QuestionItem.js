11a003889cdeb2a3ed99117d473985a4f25482cf
Zaid Akhter <zaidakhter1202@gmail.com>
2020-07-29 11:25:01 +0500

    👌 IMPROVE: Some style fixes

--- src/Components/Home/QuestionItem.js
+++ src/Components/Home/QuestionItem.js
@@ -3,19 +3,21 @@ import "./Questions.css";
 import React from "react";
 import { Tag } from "antd";
 import { Link } from "react-router-dom";
-import moment from 'moment';
+import moment from "moment";
 
 const QuestionItem = ({ id, question, tags, time }) => {
   return (
     <Link to={`/question/${id}`}>
       <div className="question-item">
-        <p>{question}</p>
-        {tags.map((tag, i) => (
-          <Tag key={i} color="processing" closable={false}>
-            {tag}
-          </Tag>
-        ))}
-        <p className="question-item-time">Asked {moment(time).fromNow()}</p>
+        <div className="main">
+          <p>{question}</p>
+          {tags.map((tag, i) => (
+            <Tag key={i} color="processing" closable={false}>
+              {tag}
+            </Tag>
+          ))}
+        </div>
+        <p className="time">Asked {moment(time).fromNow()}</p>
       </div>
     </Link>
   );
